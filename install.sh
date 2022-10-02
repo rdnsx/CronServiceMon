2db5f49a16d08860942bba68a0de64457d2ba48d
rdnsx <42463214+rdnsx@users.noreply.github.com>
2022-10-02 11:51:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,11 @@ apt update && apt upgrade -y
 #Install WireGuard
 apt install wireguard -y
 
-#Copy wg-quick config
-
 #Enable Wirequard
 systemctl enable wireguard.service
+
+#Copy wg-quick config
+cp wg0.conf /etc/wireguard/
 
 #Set crontab
 (crontab -l 2>/dev/null; echo "* * * * *./monitor_wireguard_tunnel.sh 192.168.1.1") | crontab -
