c5c53ea02e0782a55acb24bc6a9fc85e8017bd0d
rdnsx <42463214+rdnsx@users.noreply.github.com>
2022-10-02 23:14:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,8 +39,10 @@ cp wg0.conf /etc/wireguard/
 # Promts to enter Wireguard client IP and inserts it to wg0.conf line 3
 # e.g.: 10.0.73.8/24
 echo
-echo "##########################"
-echo "Enter Wireguard IP/Subnet:"
+echo
+echo "##############################################"
+echo "##############################################"
+echo "Please enter these WireGuard Client IP/Subnet:"
 read WGIP
 sed -i '3i\Address = '"$WGIP" /etc/wireguard/wg0.conf
 
@@ -50,25 +52,29 @@ sed -i '4i\PrivateKey = '"$PKEY" /etc/wireguard/wg0.conf
 
 # Shows public.key
 echo
+echo
 echo "#################################"
 echo "Here is these clients public key:"
+
 sleep 3
 echo 
 echo 
 cat /etc/wireguard/public.key
-echo "###################################"
-pause
+echo
+echo
+echo "#################################"
+read -p "Press [Enter] to continue..."
 
 # Setup WireGuard monitoring service
 echo
 echo
-echo "Setup WireGuard Monitoring"
+echo "Setup WireGuard Monitoring..."
 echo
 sleep 3
 cp monitor_wireguard_tunnel.sh /etc/wireguard/
 chmod +x /etc/wireguard/monitor_wireguard_tunnel.sh
 #Set crontab
-(crontab -l 2>/dev/null; echo "* * * * * /monitor_wireguard_tunnel.sh 192.168.1.1") | crontab -
+(crontab -l 2>/dev/null; echo "* * * * * /etc/wireguard/monitor_wireguard_tunnel.sh 192.168.1.1") | crontab -
 
 echo
 echo "#####"
