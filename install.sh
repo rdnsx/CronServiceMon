e465ced9075fcc75ed9d34d0e8d09d8d4ad1bc62
rdnsx <42463214+rdnsx@users.noreply.github.com>
2022-10-02 19:50:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-#Update OS
-apt update && apt upgrade -y
+#Update OS & clean
+apt update && apt upgrade -y && apt autoremove -y
 
 #Install WireGuard
 apt install wireguard -y
-
-#Enable Wirequard
-systemctl enable wireguard.service
+wg genkey | sudo tee /etc/wireguard/private.key
+sudo chmod go= /etc/wireguard/private.key
+sudo cat /etc/wireguard/private.key | wg pubkey | sudo tee /etc/wireguard/public.key
 
 #Copy wg-quick config
-cp wg0.conf /etc/wireguard/
+#cp wg0.conf /etc/wireguard/
+
+chmod +x monitor_wg_tunnel.sh
 
 #Set crontab
 (crontab -l 2>/dev/null; echo "* * * * *./monitor_wireguard_tunnel.sh 192.168.1.1") | crontab -
+
+#Enable Wirequard
+systemctl enable wireguard.service
