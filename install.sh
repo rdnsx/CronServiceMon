4895d81f80d86521ce4512fd883f2628c240c716
rdnsx <42463214+rdnsx@users.noreply.github.com>
2022-10-02 22:46:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 echo
+echo "###################################"
 echo "RDNSX WireGuard Installation Script"
+echo "###################################"
 sleep 3
 echo
+echo "###############"
 echo "Updating system"
+echo "###############"
 echo
 sleep 2
 
@@ -12,14 +16,18 @@ sleep 2
 apt update && apt upgrade -y && apt autoremove -y
 
 echo
+echo "####################"
 echo "Installing WireGuard"
+echo "####################"
 sleep 3
 echo
 
 #Install WireGuard
 apt install wireguard -y
 echo
+echo "###############"
 echo "Generating keys"
+echo "###############"
 sleep 3
 wg genkey | sudo tee /etc/wireguard/private.key
 sudo chmod go= /etc/wireguard/private.key
@@ -33,6 +41,8 @@ cp wg0.conf /etc/wireguard/
 
 # Promts to enter Wireguard client IP and inserts it to wg0.conf line 3
 # e.g.: 10.0.73.8/24
+echo
+echo "##########################"
 echo "Enter Wireguard IP/Subnet:"
 read WGIP
 sed -i '3i\Address = '"$WGIP" /etc/wireguard/wg0.conf
@@ -42,9 +52,14 @@ PKEY=`cat /etc/wireguard/private.key`
 sed -i '4i\PrivateKey = '"$PKEY" /etc/wireguard/wg0.conf
 
 # Shows public.key
+echo
+echo "#################################"
 echo "Here is these clients public key:"
+sleep 3
 echo 
+echo "###################################"
 cat /etc/wireguard/public.key
+echo "###################################"
 echo
 pause
 
@@ -52,7 +67,7 @@ pause
 echo
 echo "Setup WireGuard Monitoring"
 sleep 3
-cp monitor_wg_tunnel.sh /etc/wireguard/
-chmod +x /etc/wireguard/monitor_wg_tunnel.sh
+cp monitor_wireguard_tunnel.sh /etc/wireguard/
+chmod +x /etc/wireguard/monitor_wireguard_tunnel.sh
 #Set crontab
 (crontab -l 2>/dev/null; echo "* * * * * /monitor_wireguard_tunnel.sh 192.168.1.1") | crontab -
